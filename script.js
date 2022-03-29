06166c22a61cebb67980154dd77e93b7f3e54d1d
Mark Valentine <markolas@alumni.stanford.edu>
2022-03-29 13:53:35 +0100

    roll back

--- script.js
+++ script.js
@@ -173,22 +173,22 @@ $(document).ready(function () {
 
       // first we draw lines so they are behind the dots
       // phase 1 we fade out the connecting lines and fade in the lines from a single point
-      // if (phase % num_phases == 1) {
-      //   drawLine((change_frames - currentFrame) / change_frames);
-      //   drawLinesFromSinglePoint(currentFrame / change_frames);
-      // // phase 3 we fade out the single point lines and fade in the connecting lines
-      // } else if (phase % num_phases == 3) {
-      //   drawLine(currentFrame / change_frames);
-      //   drawLinesFromSinglePoint(
-      //     (change_frames - currentFrame) / change_frames
-      //   );
-      // // any phase but research, connecting lines
-      // } else if (phase % num_phases != 2) {
-      //   drawLine(1);
-      // // research, lines come from a single point
-      // } else if (phase % num_phases == 2) {
-      //   drawLinesFromSinglePoint(1);
-      // }
+      if (phase % num_phases == 1) {
+        drawLine((change_frames - currentFrame) / change_frames);
+        drawLinesFromSinglePoint(currentFrame / change_frames);
+      // phase 3 we fade out the single point lines and fade in the connecting lines
+      } else if (phase % num_phases == 3) {
+        drawLine(currentFrame / change_frames);
+        drawLinesFromSinglePoint(
+          (change_frames - currentFrame) / change_frames
+        );
+      // any phase but research, connecting lines
+      } else if (phase % num_phases != 2) {
+        drawLine(1);
+      // research, lines come from a single point
+      } else if (phase % num_phases == 2) {
+        drawLinesFromSinglePoint(1);
+      }
 
       // now we draw each dot
       // if we are in an animation phase we calcualte the easing variable and multiply it by the "velocity"
@@ -209,9 +209,8 @@ $(document).ready(function () {
         // if in a waiting phase, we draw the dot based off its formula for random motion
         // theres probably a more efficient way of calculating these positions at the start and not having to do it each cycle
         for (i = 0; i < dots.length; i++) {
-          // var position = drawDotWithJitter(dots[i]);
-          // drawDot(dots[i], position.x, position.y);
-          drawDot(dots[i]);
+          var position = drawDotWithJitter(dots[i]);
+          drawDot(dots[i], position.x, position.y);
         }
       }
 
